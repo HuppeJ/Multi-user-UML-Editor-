7fe2f418c96d0383d1fa301e94d15ed3a1126ba4
Antoine Daigneault-Demers <antoine.daigneault-demers@polymtl.ca>
2019-01-15 16:10:07 -0500

    Premier jet de la fenetre de chat

---
+++ Lourd/PolyPaint/PolyPaint/Vues/FenetreChat.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PolyPaint.Vues
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class FenetreChat : Window
+    {
+        public FenetreChat()
+        {
+            InitializeComponent();
+        }
+
+        void OnClick1(object sender, RoutedEventArgs e)
+        {
+            btn1.Background = Brushes.LightBlue;
+        }
+    }
+}
