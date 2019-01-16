74ae9004a224a35c71a39346019468a812b00be7
Mathieu <mathieu.giroux-huppe@polymtl.com>
2019-01-15 23:41:54 -0500

    Ajout d'une activite de base

--- Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/MainActivity.kt
+++ Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.polypaint.polypaint
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onGo(view: View) {
+        val intent = Intent(this, MessageListActivity::class.java)
+        startActivity(intent)
     }
 }
