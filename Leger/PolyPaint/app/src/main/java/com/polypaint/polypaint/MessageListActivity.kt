5c345253f9c2987a1437dbd646533547c08584d3
Mathieu <mathieu.giroux-huppe@polymtl.com>
2019-01-15 23:42:37 -0500

    Ajout des messsage instantanement

--- Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/MessageListActivity.kt
+++ Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/MessageListActivity.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.EditText
 
 class MessageListActivity : AppCompatActivity(){
     private var mMessageRecycler: RecyclerView? = null
     private var mMessageAdapter: MessageListAdapter? = null
+    private var messageList: ArrayList<Message> = ArrayList()
 
     override fun onCreate (savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_message_list)
 
-        var messageList: ArrayList<Message> = ArrayList();
         messageList.add(Message("allo", User("bob", 1),System.currentTimeMillis()))
         messageList.add(Message("allo", User("bob", 1),System.currentTimeMillis()))
         messageList.add(Message("allo", User("bobby", 2),System.currentTimeMillis()))
@@ -28,4 +30,10 @@ class MessageListActivity : AppCompatActivity(){
         mMessageRecycler?.adapter = mMessageAdapter
     }
 
+    fun sendMessage(view: View){
+
+        var editText = findViewById<EditText>(R.id.edittext_chatbox)
+        messageList.add(Message( editText.text.toString() , User("bob", 1), System.currentTimeMillis()))
+        mMessageAdapter?.notifyDataSetChanged()
+    }
 }
