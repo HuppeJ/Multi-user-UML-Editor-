d2279c47a8d227537a4da13fce0ce31a5e6cf317
HuppeJ <jeremie.huppe@gmail.com>
2019-01-25 17:30:01 -0500

    Added documentation for SocketEvents file

--- Serveur/SocketEvents.js
+++ Serveur/SocketEvents.js
@@ -1,7 +1,11 @@
 module.exports = Object.freeze({
+    // X = any events name
+    // Server listens to X ex.: CREATE_USER
+    // Server emits X_RESPONSE ex.: CREATE_USER_RESPONSE
+
     // Authentication 
     CREATE_USER: "createUser", 
-    CREATE_USER_RESPONSE: "createUserResponse", // boolean: isUserCreated
+    CREATE_USER_RESPONSE: "createUserResponse",
     LOGIN_USER: "loginUser",
     LOGIN_USER_RESPONSE: "loginUserResponse"
 });
