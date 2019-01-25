e44c62e1be1ebe01bd9134b4d63c16245a06b67f
HuppeJ <jeremie.huppe@gmail.com>
2019-01-25 16:02:36 -0500

    Fixed data transfer in emit

--- Serveur/services/authentication/authenticationSocketEvents.js
+++ Serveur/services/authentication/authenticationSocketEvents.js
@@ -5,27 +5,35 @@ const userAccountManager = UserAccountManager();
 
 module.exports = (io) => {
     io.on('connection', function (client) {
+        // TODO : remove
         client.on('test', function () {
             client.emit('hello');
         });
 
         client.on('createUser', function (data) {
-            console.log("Username: " + data.username);
-            console.log("Password: " + data.password);
+            let isUserCreated = false;
             if (userAccountManager.isUsernameAvailable(data.username)) {
                 userAccountManager.addUser(data.username, data.password);
-                client.emit('userCreated', data.username);
+                isUserCreated = true;
             }
-            else
-                client.emit('usernameUnavailable', data.username);
+            const response = {
+                isUserCreated: isUserCreated,
+                message: isUserCreated ? `User ${data.username} has been created` : `${data.username} is not valid.`
+            }
+            client.emit('createUserResponse', response);
         });
 
         client.on('loginUser', function (username, password) {
+            let isLoginSuccessful = false;
             if (userAccountManager.authenticateUser(username, password)) {
-                client.emit('loginSuccessful');
+                isLoginSuccessful = true;
             }
-            else
-                client.emit('loginFailed');
+            const response = {
+                isLoginSuccessful: isLoginSuccessful,
+                message: isLoginSuccessful ? `Login Successful.` : `Login not Successful.`
+            }
+
+            client.emit('loginUserResponse', response);
         });
     })
 };
