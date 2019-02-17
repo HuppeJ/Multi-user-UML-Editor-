766977defb65295d0eb8d166521302442c196331
JeHuppe <jeremie.huppe@gmail.com>
2019-02-17 15:09:34 -0500

    upgraded basic element xml

--- Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/Activity/DrawingActivity.kt
+++ Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/Activity/DrawingActivity.kt
@@ -5,29 +5,29 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.RelativeLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.github.nkzawa.socketio.client.Socket
 import com.polypaint.polypaint.Application.PolyPaint
 import com.polypaint.polypaint.Model.BasicElement
 import com.polypaint.polypaint.R
+import kotlinx.android.synthetic.main.activity_drawing.*
+import kotlinx.android.synthetic.main.basic_element.*
 
 class DrawingActivity : AppCompatActivity(){
-    private var parentRelativeLayout : RelativeLayout? = null
+    // private var parentRelativeLayout : RelativeLayout? = null
     private var inflater : LayoutInflater? = null
     private var childIndexCount : Int = 0
-    private var childSelected : Int = 0
+    public var childSelected : Int = 0
+
+
+
     //TODO: déplacer dans object contenant tous les eventListeners possible
     private var onTouchListener = View.OnTouchListener { v, event ->
-        //
-        v.x = event.rawX - v.width/2 - parentRelativeLayout!!.x
-        v.y = event.rawY - v.height - parentRelativeLayout!!.y
+        v.x = event.rawX - v.width/2 - parent_relative_layout!!.x
+        v.y = event.rawY - v.height - parent_relative_layout!!.y
         //Set soft focus
         childSelected = v.id
 
@@ -35,9 +35,9 @@ class DrawingActivity : AppCompatActivity(){
         // Return true means this listener has complete process this event successfully.
         true
     }
+
     private var onLongClickListener = View.OnLongClickListener { v ->
-        v.findViewById<TextView>(R.id.first_line).text = "LONGCLICK"
-        v.findViewById<TextView>(R.id.first_line).text = "ACTION"
+        first_line.text = "LONGCLICK"
         true
     }
     /*
@@ -53,11 +53,9 @@ class DrawingActivity : AppCompatActivity(){
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate (savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         setContentView(R.layout.activity_drawing)
-
-        parentRelativeLayout = findViewById(R.id.parent_layout)
-        //parentRelativeLayout!!.setOnClickListener(onTouchListenerRelLayout)
 
         inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
@@ -72,16 +70,16 @@ class DrawingActivity : AppCompatActivity(){
             val viewToAdd = inflater!!.inflate(R.layout.basic_element, null)
             mBasicElem.addView(viewToAdd,mBasicElem.childCount - 1)
 
-            mBasicElem.setOnTouchListener(onTouchListener)
-            mBasicElem.setOnLongClickListener(onLongClickListener)
-            parentRelativeLayout?.addView(mBasicElem, childIndexCount)
+           mBasicElem.setOnTouchListener(onTouchListener)
+//            mBasicElem.setOnLongClickListener(onLongClickListener)
+            parent_relative_layout?.addView(mBasicElem, childIndexCount)
             childIndexCount++
         }
-        var moveButton: Button = findViewById(R.id.move_button)
-        moveButton.setOnClickListener {
-            val txt = parentRelativeLayout?.getChildAt(childSelected)!!.findViewById<TextView>(R.id.first_line).text
-            parentRelativeLayout?.getChildAt(childSelected )!!.findViewById<TextView>(R.id.first_line).text = txt.toString() + "3"
-            parentRelativeLayout?.getChildAt(childSelected )!!.findViewById<TextView>(R.id.second_line).text = "TEXT4"
+
+        move_button.setOnClickListener {
+//            val txt = parentRelativeLayout?.getChildAt(childSelected)!!.first_line.text
+//            parentRelativeLayout?.getChildAt(childSelected )!!.findViewById<TextView>(R.id.first_line).text = txt.toString() + "3"
+//            parentRelativeLayout?.getChildAt(childSelected )!!.findViewById<TextView>(R.id.second_line).text = "TEXT4"
         }
     }
 
--- Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/Model/BasicElement.kt
+++ Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/Model/BasicElement.kt
@@ -1,18 +1,66 @@
 package com.polypaint.polypaint.Model
 
 import android.content.Context
+import android.view.MotionEvent
 import android.view.View
 import android.widget.RelativeLayout
+import kotlinx.android.synthetic.main.activity_drawing.view.*
+import kotlinx.android.synthetic.main.basic_element.view.*
 
-class BasicElement(context: Context) : RelativeLayout(context) {
+class BasicElement: RelativeLayout, View.OnTouchListener {
 
     var xRel : Float = 0F
     var yRel : Float = 0F
     var mId : Int = -1
 
-    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        super.onLayout(changed, l, t, r, b)
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    fun init(context: Context) {
+        setOnTouchListener(this)
+//        dragHandle = this.findViewById(R.id.drag_handle)
+        //dragHandle = this.findViewById(R.id.resizableLayout)
+
+    }
+    override fun onTouch(view: View, motionEvent: MotionEvent): Boolean {
+        first_line.text = "aa";
+
+        // view.x = motionEvent.rawX - view.width/2// - parent_relative_layout!!.x
+        // view.y = motionEvent.rawY - view.height// - parent_relative_layout!!.y
+        //Set soft focus
+
+//        view.requestFocus()
+
+//
+//        val action = motionEvent.action
+//        if (action == MotionEvent.ACTION_DOWN) {
+//            // detect if tap is on handles
+//            downRawY = motionEvent.rawY
+//            height = this.measuredHeight.toFloat()
+//        } else {
+//            // change layout margin inside switch
+//            when (side) {
+//
+//            }
+//            val parent = this.parent as View
+//            if (downRawY < parent.height - height + 2 * dragHandle!!.height) {
+//                val rawY =
+//                    if (motionEvent.rawY > 20 * dragHandle!!.height) motionEvent.rawY else 20 * dragHandle!!.height
+//                val p = this.layoutParams as ViewGroup.MarginLayoutParams
+////                p.topMargin = rawY.toInt()
+//                p.topMargin = 100
+//                if (p.topMargin != 0)
+//                //this.top_margine = p.topMargin
+//                    this.layoutParams = p
+//            }
+//        }
+        return false
+    }
+
+    override fun onInterceptTouchEvent(motionEvent: MotionEvent): Boolean {
+        onTouch(this, motionEvent)
+        return false
     }
 
 }
