309db3c324f1c1ed94fb70f1d6039f1742983ec1
AlexandreRault <alexandre.rault@polymtl.ca>
2019-02-22 13:34:19 -0500

    customInkCanvas override

--- Lourd/PolyPaint/PolyPaint/CustomInk/CustomInkCanvas.cs
+++ Lourd/PolyPaint/PolyPaint/CustomInk/CustomInkCanvas.cs
@@ -9,6 +9,16 @@ namespace PolyPaint.CustomInk
 {
     class CustomInkCanvas : InkCanvas
     {
+        protected override void OnStrokeCollected(InkCanvasStrokeCollectedEventArgs e)
+        {
+            // Remove the original stroke and add a custom stroke.
+            this.Strokes.Remove(e.Stroke);
+            CustomStroke customStroke = new CustomStroke(e.Stroke.StylusPoints);
+            this.Strokes.Add(customStroke);
 
+            // Pass the custom stroke to base class' OnStrokeCollected method.
+            InkCanvasStrokeCollectedEventArgs args = new InkCanvasStrokeCollectedEventArgs(customStroke);
+            base.OnStrokeCollected(args);
+        }
     }
 }
