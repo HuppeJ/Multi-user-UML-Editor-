4fce9a789eae20e9feea8aa48e2a00b33fb79be7
genevieveBock <genevieve-bock@hotmail.com>
2019-02-26 11:07:34 -0500

    added templates for collaborative drawing

---
+++ Lourd/PolyPaint/PolyPaint/Templates/LinkStyle.cs
@@ -0,0 +1,9 @@
+﻿namespace PolyPaint.Templates
+{
+    class LinkStyle
+    {
+        public string color { get; set; }
+        public int thickness { get; set; }
+        public int type { get; set; }
+    }
+}
