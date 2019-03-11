621487c9281b426d638a9e558fa886467a824f6e
GenevieveBock1 <genevieve.bock@polymtl.ca>
2019-03-11 18:00:59 -0400

    added constructors to communication Templates

--- Lourd/PolyPaint/PolyPaint/Templates/AnchorPoint.cs
+++ Lourd/PolyPaint/PolyPaint/Templates/AnchorPoint.cs
@@ -4,5 +4,17 @@
     {
         public string formId { get; set; }
         public int anchor { get; set; }
+        public string multiplicity { get; set; }
+
+        public AnchorPoint()
+        {
+        }
+
+        public AnchorPoint(string formId, int anchor, string multiplicity)
+        {
+            this.formId = formId;
+            this.anchor = anchor;
+            this.multiplicity = multiplicity;
+        }
     }
 }
--- Lourd/PolyPaint/PolyPaint/Templates/Link.cs
+++ Lourd/PolyPaint/PolyPaint/Templates/Link.cs
@@ -5,10 +5,26 @@ namespace PolyPaint.Templates
     public class Link
     {
         public string id { get; set; }
+        public string name { get; set; }
         public AnchorPoint from { get; set; }
         public AnchorPoint to { get; set; }
         public int type { get; set; }
         public LinkStyle style { get; set; }
         public List<Coordinates> path { get; set; }
+
+        public Link()
+        {
+        }
+
+        public Link(string id, string name, AnchorPoint from, AnchorPoint to, int type, LinkStyle style, List<Coordinates> path)
+        {
+            this.id = id;
+            this.name = name;
+            this.from = from;
+            this.to = to;
+            this.type = type;
+            this.style = style;
+            this.path = path;
+        }
     }
 }
