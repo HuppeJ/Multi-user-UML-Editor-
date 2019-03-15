bbdf99611a3c6954ac36554a2adcc519f0ebb069
Mathieu <mathieu.giroux-huppe@polymtl.ca>
2019-03-15 13:33:49 -0400

    Create the different types of links

--- Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/View/LinkView.kt
+++ Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/View/LinkView.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
 import com.github.nkzawa.socketio.client.Socket
 import com.polypaint.polypaint.Application.PolyPaint
+import com.polypaint.polypaint.Enum.LinkTypes
 import com.polypaint.polypaint.Fragment.EditLinkDialogFragment
 import com.polypaint.polypaint.Holder.ViewShapeHolder
 import com.polypaint.polypaint.Model.Coordinates
@@ -68,17 +69,17 @@ class LinkView: View{
         val angle : Double = Math.atan2( (end.y - start.y), (end.x - start.x))
         val angle2: Double =  angle -Math.PI/2
 
-        var thickness: Int = 10
+        var thickness: Float = 10f
         if( link?.style?.thickness != null) {
-            thickness = link?.style?.thickness!!.toInt()
+            thickness = link?.style?.thickness!!.toFloat()
         }
 
         val path: Path = Path()
-        path.moveTo(start.x.toFloat(), start.y.toFloat())
-        path.lineTo(end.x.toFloat(), end.y.toFloat())
-        path.lineTo(end.x.toFloat() + thickness.toFloat() * Math.cos(angle2).toFloat(), end.y.toFloat()+thickness.toFloat() * Math.sin(angle2).toFloat())
-        path.lineTo(start.x.toFloat() + thickness.toFloat() * Math.cos(angle2).toFloat(), start.y.toFloat()+thickness.toFloat() * Math.sin(angle2).toFloat())
-        path.lineTo(start.x.toFloat(), start.y.toFloat())
+        path.moveTo(start.x.toFloat() - thickness/2 * Math.cos(angle2).toFloat(), start.y.toFloat()-thickness/2 * Math.sin(angle2).toFloat())
+        path.lineTo(end.x.toFloat() - thickness/2 * Math.cos(angle2).toFloat(), end.y.toFloat()-thickness/2 * Math.sin(angle2).toFloat())
+        path.lineTo(end.x.toFloat() + thickness/2 * Math.cos(angle2).toFloat(), end.y.toFloat()+thickness/2 * Math.sin(angle2).toFloat())
+        path.lineTo(start.x.toFloat() + thickness/2 * Math.cos(angle2).toFloat(), start.y.toFloat()+thickness/2 * Math.sin(angle2).toFloat())
+        path.lineTo(start.x.toFloat() - thickness/2 * Math.cos(angle2).toFloat(), start.y.toFloat()-thickness/2 * Math.sin(angle2).toFloat())
         path.close()
         canvas.drawPath(path, paint)
 
@@ -89,6 +90,103 @@ class LinkView: View{
         region.setPath(path, Region(rect.left.toInt(), rect.top.toInt(), rect.right.toInt(), rect.bottom.toInt()))
 
 
+
+        when(link?.type){
+            LinkTypes.AGGREGATION.ordinal->drawAggregation(canvas,angle, thickness)
+            LinkTypes.COMPOSITION.ordinal->drawComposition(canvas,angle, thickness)
+            LinkTypes.HERITAGE.ordinal -> drawHeritage(canvas, angle2, thickness)
+            LinkTypes.ONE_WAY_ASSOCIATION.ordinal->drawArrowTip(canvas, angle, thickness, false)
+            LinkTypes.TWO_WAY_ASSOCIATION.ordinal->{
+                drawArrowTip(canvas,angle,thickness, true)
+                drawArrowTip(canvas, angle, thickness, false)
+            }
+        }
+
+        addTextViews(parent)
+
+    }
+
+    private fun drawComposition(canvas: Canvas, lineAngle: Double, thickness: Float){
+        val diamondPaint = Paint()
+        diamondPaint.color = paint.color
+        diamondPaint.strokeWidth = thickness
+        diamondPaint.style = Paint.Style.FILL_AND_STROKE
+        drawDiamond(canvas, lineAngle, diamondPaint)
+    }
+    private fun drawAggregation(canvas: Canvas, lineAngle: Double, thickness: Float){
+        val diamondPaint = Paint()
+        diamondPaint.color = paint.color
+        diamondPaint.strokeWidth = thickness
+        diamondPaint.style = Paint.Style.STROKE
+        drawDiamond(canvas, lineAngle, diamondPaint)
+    }
+    private fun drawDiamond(canvas: Canvas, lineAngle: Double, paint: Paint){
+        val diamondLeftAngle: Double = lineAngle - Math.PI/4
+        val diamondRightAngle: Double = lineAngle + Math.PI/4
+        val leftPoint: Point = Point((end.x + 45 * Math.cos(diamondLeftAngle)).toInt(), (end.y+45 * Math.sin(diamondLeftAngle)).toInt() )
+        val middlePoint: Point = Point((leftPoint.x + 45 * Math.cos(diamondRightAngle)).toInt(), (leftPoint.y+45 * Math.sin(diamondRightAngle)).toInt() )
+        val rightPoint: Point = Point((end.x + 45 * Math.cos(diamondRightAngle)).toInt(), (end.y+45 * Math.sin(diamondRightAngle)).toInt() )
+        val arrowPath: Path = Path()
+        arrowPath.moveTo(end.x.toFloat(), end.y.toFloat())
+        arrowPath.lineTo(leftPoint.x.toFloat(), leftPoint.y.toFloat())
+        arrowPath.lineTo(middlePoint.x.toFloat(), middlePoint.y.toFloat())
+        arrowPath.lineTo(rightPoint.x.toFloat(), rightPoint.y.toFloat())
+        arrowPath.lineTo(end.x.toFloat(), end.y.toFloat())
+        arrowPath.close()
+        canvas.drawPath(arrowPath, paint)
+    }
+
+    private fun drawHeritage(canvas: Canvas, perpendicularAngle: Double, thickness: Float){
+        val arrowPaint = Paint()
+        arrowPaint.color = paint.color
+        arrowPaint.strokeWidth = thickness
+        arrowPaint.style = Paint.Style.STROKE
+        val leftAngle: Double = perpendicularAngle + 2*Math.PI/3
+       // val rightAngle: Double = perpendicularAngle + Math.PI/3
+        val leftPoint: Point = Point((end.x + 30 * Math.cos(perpendicularAngle)).toInt(), (end.y+30 * Math.sin(perpendicularAngle)).toInt() )
+        val middlePoint: Point = Point((leftPoint.x + 60 * Math.cos(leftAngle)).toInt(), (leftPoint.y+60 * Math.sin(leftAngle)).toInt() )
+        val rightPoint: Point = Point((end.x - 30 * Math.cos(perpendicularAngle)).toInt(), (end.y - 30 * Math.sin(perpendicularAngle)).toInt() )
+        val arrowPath: Path = Path()
+        arrowPath.moveTo(end.x.toFloat(), end.y.toFloat())
+        arrowPath.lineTo(leftPoint.x.toFloat(), leftPoint.y.toFloat())
+        arrowPath.lineTo(middlePoint.x.toFloat(), middlePoint.y.toFloat())
+        arrowPath.lineTo(rightPoint.x.toFloat(), rightPoint.y.toFloat())
+        arrowPath.lineTo(end.x.toFloat(), end.y.toFloat())
+        arrowPath.close()
+        canvas.drawPath(arrowPath, arrowPaint)
+    }
+
+    private fun drawArrowTip(canvas: Canvas, lineAngle: Double, thickness: Float, isStart: Boolean){
+        // draw a tip to the link
+
+        val arrowPaint = Paint()
+        arrowPaint.color = paint.color
+        arrowPaint.strokeWidth = thickness
+        arrowPaint.style = Paint.Style.STROKE
+
+        var arrowLeftAngle: Double
+        var arrowRightAngle: Double
+        var middlePoint: Point
+        if(isStart){
+            arrowLeftAngle = lineAngle - Math.PI/4
+            arrowRightAngle = lineAngle + Math.PI/4
+            middlePoint = Point(start.x.toInt(), start.y.toInt())
+        } else {
+            arrowLeftAngle = lineAngle - 3*Math.PI/4
+            arrowRightAngle = lineAngle + 3*Math.PI/4
+            middlePoint = Point(end.x.toInt(), end.y.toInt())
+        }
+
+        val arrowPath: Path = Path()
+        arrowPath.moveTo(middlePoint.x.toFloat(), middlePoint.y.toFloat())
+        arrowPath.lineTo(middlePoint.x.toFloat() + 45 * Math.cos(arrowLeftAngle).toFloat(), middlePoint.y.toFloat()+45 * Math.sin(arrowLeftAngle).toFloat())
+        arrowPath.moveTo(middlePoint.x.toFloat(), middlePoint.y.toFloat())
+        arrowPath.lineTo(middlePoint.x.toFloat() + 45 * Math.cos(arrowRightAngle).toFloat(), middlePoint.y.toFloat()+45 * Math.sin(arrowRightAngle).toFloat())
+        arrowPath.close()
+        canvas.drawPath(arrowPath, arrowPaint)
+    }
+
+    private fun addTextViews(parent: RelativeLayout){
         multiplicityFrom = TextView(context)
         multiplicityFrom?.x = start.x.toFloat() +15
         multiplicityFrom?.y = start.y.toFloat()
@@ -102,8 +200,8 @@ class LinkView: View{
         parent.addView(multiplicityTo)
 
         nameView = TextView(context)
-        nameView?.x =start.x.toFloat() + Math.abs(end.x.toFloat() - start.x.toFloat()) /2  +15
-        nameView?.y =start.y.toFloat() + Math.abs(end.y.toFloat() - start.y.toFloat()) /2
+        nameView?.x =start.x.toFloat() + (end.x.toFloat() - start.x.toFloat()) /2  +15
+        nameView?.y =start.y.toFloat() + (end.y.toFloat() - start.y.toFloat()) /2
         nameView?.setText(link?.name)
         parent.addView(nameView)
     }
