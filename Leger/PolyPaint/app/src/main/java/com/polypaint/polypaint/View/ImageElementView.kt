208bc57ec77773100b76ed126a81a43106bc5ef6
JeHuppe <jeremie.huppe@gmail.com>
2019-03-16 15:51:42 -0400

    Added new shapes

--- Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/View/ImageElementView.kt
+++ Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/View/ImageElementView.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageView
 import android.widget.RelativeLayout
 import androidx.fragment.app.DialogFragment
 import com.polypaint.polypaint.Enum.ShapeTypes
@@ -16,8 +17,8 @@ import kotlinx.android.synthetic.main.view_class.view.*
 
 
 class ImageElementView(context: Context, shapeType: ShapeTypes): BasicElementView(context) {
-    override var mMinimumWidth : Float = 300F
-    override var mMinimumHeight : Float = 370F
+    override var mMinimumWidth : Float = 220F
+    override var mMinimumHeight : Float = 320F
     private var shapeType : ShapeTypes? = shapeType
 
 
@@ -45,20 +46,31 @@ class ImageElementView(context: Context, shapeType: ShapeTypes): BasicElementVie
 
         borderResizableLayout.addView(child)
 
+        this.resize(mMinimumWidth.toInt(), mMinimumHeight.toInt())
+
+
+        // TODO : On ne peut pas mettre à jour les dimmenssion d'une vue dans onAttachedToWindow alors le code ci-dessous ne fait rien en ce moment
+        // TODO : Pour le moment je n'ai pas trouvé d'alternative qui fonctionne sans interférer avec le resize.
+        borderResizableLayout.layoutParams.width =  (mMinimumWidth).toInt()
+
         borderResizableLayout.layoutParams.height = (mMinimumHeight).toInt()
-        linearLayoutCompat.layoutParams.height = (mMinimumHeight/7).toInt()
-        linearLayoutCompat2.layoutParams.height = (3*mMinimumHeight/7).toInt()
+        linearLayoutCompat.layoutParams.height = (9*mMinimumHeight/10).toInt()
+        linearLayoutCompat2.layoutParams.height = (1*mMinimumHeight/10).toInt()
+
     }
 
     override fun resize(newWidth:Int, newHeight:Int){
+        Log.d("newWidth", newWidth.toString())
+        Log.d("newHeight", newHeight.toString())
+
         if(newWidth >= mMinimumWidth){
             borderResizableLayout.layoutParams.width = newWidth
         }
 
         if(newHeight >= mMinimumHeight){
             borderResizableLayout.layoutParams.height = newHeight
-            linearLayoutCompat.layoutParams.height = (newHeight / 7)
-            linearLayoutCompat2.layoutParams.height = (3 * newHeight / 7)
+            linearLayoutCompat.layoutParams.height = (9*newHeight / 10)
+            linearLayoutCompat2.layoutParams.height = (1 * newHeight / 10)
         }
 
         borderResizableLayout.requestLayout()
