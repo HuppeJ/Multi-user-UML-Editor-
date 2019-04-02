934a629bc409a3ecb8bc460276f440754c2587a8
Mathieu Giroux-Huppe <mathieu.giroux-huppe@polymtl.ca>
2019-04-02 15:49:32 -0400

    Updated the link modal to prevent empty colors

--- Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/Model/LinkStyle.kt
+++ Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/Model/LinkStyle.kt
@@ -1,4 +1,4 @@
 package com.polypaint.polypaint.Model
 import java.io.Serializable
 
-class LinkStyle(var color: String, var thickness: Int, var type: Int): Serializable{}
+class LinkStyle(var color: String = "#FF000000", var thickness: Int, var type: Int): Serializable{}
