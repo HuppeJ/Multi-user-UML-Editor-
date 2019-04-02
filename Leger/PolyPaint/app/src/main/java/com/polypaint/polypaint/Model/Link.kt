9aadb353be9c9aa35748dd79167c74c2f32c0568
Maxime-Gosselin <maxime.gosselin@polymtl.ca>
2019-04-02 15:41:29 -0400

    Fix Duplicate Link Path

--- Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/Model/Link.kt
+++ Leger/PolyPaint/app/src/main/java/com/polypaint/polypaint/Model/Link.kt
@@ -2,9 +2,18 @@ package com.polypaint.polypaint.Model
 
 import java.io.Serializable
 import java.util.*
+import kotlin.collections.ArrayList
 
 class Link(var id: String, var name: String, var from: AnchorPoint, var to: AnchorPoint, var type: Int, var style: LinkStyle, var path: ArrayList<Coordinates>) : Serializable, DrawingElement(id){
 
 
-    fun copy(id: String = UUID.randomUUID().toString(), name: String= this.name, from: AnchorPoint = AnchorPoint(), to: AnchorPoint = AnchorPoint(), type:Int = this.type, style: LinkStyle=this.style.copy(), path: ArrayList<Coordinates> = this.path.clone() as ArrayList<Coordinates>) = Link(id,name, from, to,type,style,path)
+    fun copy(id: String = UUID.randomUUID().toString(), name: String= this.name, from: AnchorPoint = AnchorPoint(), to: AnchorPoint = AnchorPoint(), type:Int = this.type, style: LinkStyle=this.style.copy(), path: ArrayList<Coordinates> = copyPathCoordinates()) = Link(id,name, from, to,type,style,path)
+
+    private fun copyPathCoordinates():ArrayList<Coordinates>{
+        var pathCopied : ArrayList<Coordinates> = ArrayList()
+        for(coordinate in this.path){
+            pathCopied.add(coordinate.copy())
+        }
+        return pathCopied
+    }
 }
