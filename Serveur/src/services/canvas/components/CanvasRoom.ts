c1200cbad9f11e79858b0a3f08371796dc6b9322
HuppeJ <jeremie.huppe@gmail.com>
2019-04-05 14:43:20 -0400

    fixed selected links

--- Serveur/src/services/canvas/components/CanvasRoom.ts
+++ Serveur/src/services/canvas/components/CanvasRoom.ts
@@ -438,7 +438,7 @@ export default class CanvasRoom {
         })
 
         return JSON.stringify({
-            selectedLinks: Array.from(this.selectedLinks.keys())
+            selectedLinks: filtered
         });
     }
 
