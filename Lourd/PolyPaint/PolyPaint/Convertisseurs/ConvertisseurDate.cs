ce548eea17b0a947bc72d0b865a0838a3ab8da03
Antoine Daigneault-Demers <antoine.daigneault-demers@polymtl.ca>
2019-04-06 16:51:57 -0400

    Added time to history entry

---
+++ Lourd/PolyPaint/PolyPaint/Convertisseurs/ConvertisseurDate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PolyPaint.Convertisseurs
+{
+    public class ConvertisseurDate: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string timeString = value as string;
+            long time = long.Parse(timeString);
+
+            return value = DateTimeOffset.FromUnixTimeMilliseconds(time).DateTime;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (string)value;
+        }
+
+    }
+}
