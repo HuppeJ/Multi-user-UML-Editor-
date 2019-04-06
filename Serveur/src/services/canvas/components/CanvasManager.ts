66c1eddd8f7b49c73674b7c492237e3701724be6
JeHuppe <jeremie.huppe@gmail.com>
2019-04-06 16:49:05 -0400

    removed unused logs

--- Serveur/src/services/canvas/components/CanvasManager.ts
+++ Serveur/src/services/canvas/components/CanvasManager.ts
@@ -150,8 +150,6 @@ export default class CanvasManager {
 
     public getCanvasRoomFromUsername(unsername: any): string {
         for (const [canvasRoomId, canvasRoom] of this.canvasRooms.entries()) {
-            console.log(canvasRoom.getConnectedUsersSERI())
-
             if (canvasRoom.hasUser(unsername)) {
                 return canvasRoomId;
             }
